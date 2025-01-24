4cef8cb92b61e9d0599ef4553c5d1190d79a2898
unknown <hatimerrattab@gmail.com>
2025-01-24 15:09:17 +0100

    Your commit message here

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,6 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
     static associate(models) {
-      // Define associations here (if any)
     }
   }
   Category.init(
